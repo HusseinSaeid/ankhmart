a333b78bd92ee691e8810ded767a3db8a1f8931c
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2025-10-13 20:59:57 +0300

    fix

--- src/app/(app)/(home)/search-filters/SubCategoryMenu.tsx
+++ src/app/(app)/(home)/search-filters/SubCategoryMenu.tsx
@@ -24,7 +24,7 @@ export const SubCategoryMenu = ({ category, isOpen, position }: props) => {
       <div className="h-3 w-60"></div>
       <div
         style={{ backgroundColor }}
-        className="w-60 text-black rounded-md overflow-hidden border  shadow-[4px_4px_0px_0 px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
+        className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
       >
         <div>
           {category.subcategories?.map((subcategories: Category) => (
