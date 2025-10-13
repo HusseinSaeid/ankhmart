7b3755cdd28503f71357bd72c16049b2404320cc
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2025-10-13 14:57:51 +0300

    5-search input and categories

---
+++ src/app/(app)/(home)/search-filters/CategoryDropdown.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState, useRef } from "react";
+
+import { Category } from "@/payload-types";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useDropDownPosition } from "./useDropDownPosition";
+import { SubCategoryMenu } from "./SubCategoryMenu";
+
+interface Props {
+  category: Category;
+  isActive: boolean;
+  isNavigationHovered: boolean;
+}
+
+export const CategoryDropdown = ({
+  category,
+  isActive,
+  isNavigationHovered,
+}: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropDownRef = useRef<HTMLDivElement>(null);
+  const { getDropDownPosition } = useDropDownPosition(dropDownRef);
+
+  const onMouseEnter = () => {
+    if (category.subcategories) setIsOpen(true);
+  };
+  const onMouseLeave = () => {
+    setIsOpen(false);
+  };
+  const dropDownPosition = getDropDownPosition();
+  return (
+    <div
+      className="relative"
+      ref={dropDownRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <div className="relative">
+        <Button
+          variant={"elevated"}
+          className={cn(
+            "h-11 px-4 bg-transparent border-transparent  hover:bg-white hover:border-primary text-black",
+            isActive && isNavigationHovered && "bg-white border-primary"
+          )}
+        >
+          <span className="capitalize">{category.name}</span>
+        </Button>
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div
+            className={cn(
+              "opacity-0 absolute w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+              isOpen && "opacity-100"
+            )}
+            style={{ top: "calc(100% + 2px)" }} // 8px offset تحت العنصر الأب
+          />
+        )}
+      </div>
+      <SubCategoryMenu
+        category={category}
+        isOpen={isOpen}
+        position={dropDownPosition}
+      />
+    </div>
+  );
+};
