f4bb1a72c96b9fde7a7abf46f51fac98c372618f
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2025-10-25 00:26:04 +0300

    fix

--- src/modules/products/server/procedure.ts
+++ src/modules/products/server/procedure.ts
@@ -14,16 +14,10 @@ export const productsRouter = createTRPCRouter({
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
-      if (input.minPrice) {
+      if (input.minPrice || input.maxPrice) {
         where.price = {
-          ...where.price,
-          greater_than_equal: input.minPrice,
-        };
-      }
-      if (input.maxPrice) {
-        where.price = {
-          ...where.price,
-          less_than_equal: input.maxPrice,
+          ...(input.minPrice && { greater_than_equal: input.minPrice }),
+          ...(input.maxPrice && { less_than_equal: input.maxPrice }),
         };
       }
 
