d55d7e7133804a88f95f87f13c05d3f867857dc0
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2025-10-25 00:13:19 +0300

    Update src/modules/products/ui/components/priceFilter.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/products/ui/components/priceFilter.tsx
+++ src/modules/products/ui/components/priceFilter.tsx
@@ -10,13 +10,10 @@ interface PriceFilterProps {
 }
 export const formatAsCurrency = (value: string) => {
   const numericValue = value.replace(/[^0-9.]/g, "");
-  const parts = numericValue.split(".");
-  const formattedValue =
-    parts[0] + (parts.length > 1 ? "." + parts[1]?.slice(0, 2) : "");
-  if (!formattedValue) return "";
-  const numberValue = parseFloat(formattedValue);
+  if (!numericValue) return "";
+  const numberValue = parseFloat(numericValue);
   if (isNaN(numberValue)) return "";
-  return new Intl.NumberFormat("en-Us", {
+  return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "EGP",
     minimumFractionDigits: 0,
